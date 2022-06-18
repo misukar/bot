890309bb793369e102f575dfbeeba719562b7c63
misukar <107444812+misukar@users.noreply.github.com>
2022-06-18 18:38:19 +0700

    Update ainetoxic.js

--- plugins/ainetoxic.js
+++ plugins/ainetoxic.js
@@ -11,7 +11,7 @@ conn.sendFile(m.chat, gaboleh, '', '', m, true)
 // await conn.sendMessage(m.chat, { audio: { url: helloaine }, mimetype: 'audio/mp4'}, m)
 }
 
-handler.customPrefix = /^(kontol|memek|jembut|jancok|ngentod)$/i
+handler.customPrefix = /^(kntl|kontol|mmk|meki|memek|embut|jmbt|jembut|asu|jancok|ngen|ngtd|entod|ngentod)$/i
 handler.command = new RegExp
 
 handler.limit = true
