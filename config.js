d392f2c01ace33f7df552c98aed0ccf785c892c6
misukar <107444812+misukar@users.noreply.github.com>
2022-06-18 10:03:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
+  ['628818507997'],
   ['62895330379186'],
   ['62895330379186', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = '@mamankbot'
+  var sticker_author = 'Fauzan'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Aine'
+global.wm = '© Fauzan'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
